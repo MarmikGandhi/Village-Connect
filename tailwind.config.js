ace3b69ad839b58796ac1d5119dbdba01ffa2138
MarmikGandhi <marmikgandhi@gmail.com>
2025-08-15 04:10:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#047857',
+        accent: '#3B82F6',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
